890899d0153fda0ab412d09f272a62705523b86f
CBhangs <94818557+CBhangs@users.noreply.github.com>
2022-03-03 21:03:41 -0500

    updated

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,11 +1,13 @@
 const pokemons = [
-    {name: "bulbasaur", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"},
-    {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
-    {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
-    {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
-    {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
-    {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
-    {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
+    {name: "bulbasaur", img: "https://archives.bulbagarden.net/media/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png"},
+    {name: "ivysaur", img: "https://archives.bulbagarden.net/media/upload/thumb/7/73/002Ivysaur.png/500px-002Ivysaur.png"},
+    {name: "venusaur", img: "https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:eco%2Cw_1200/MTc2MjYwODQ5NTk2NTcyODYy/pokemon-venusaur-nicknames.png"},
+    {name: "charmander", img: "https://archives.bulbagarden.net/media/upload/thumb/7/73/004Charmander.png/500px-004Charmander.png"},
+    {name: "charmeleon", img: "https://archives.bulbagarden.net/media/upload/thumb/4/4a/005Charmeleon.png/500px-005Charmeleon.png"},
+    {name: "charizard", img: "https://archives.bulbagarden.net/media/upload/thumb/7/7e/006Charizard.png/1200px-006Charizard.png"},
+    {name: "squirtle", img: "https://archives.bulbagarden.net/media/upload/3/39/007Squirtle.png"},
+    {name: "wartortle", img: "https://archives.bulbagarden.net/media/upload/0/0c/008Wartortle.png"},
+    {name: "blastoise", img: "https://archives.bulbagarden.net/media/upload/thumb/0/02/009Blastoise.png/1200px-009Blastoise.png"}
  ];
 
 
